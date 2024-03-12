dc40fdc28300986ef21a7ee0f964a132d7c91fb9
GIgaSora <krittayoch_jo@cmu.ac.th>
2024-03-12 23:44:13 +0700

    basic design

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import Home from '@/views/Home.vue'
+import Login from '@/views/Login.vue'
+import Register from "@/views/Register.vue"
+import Detail from "@/views/Detail.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
+    },
+    {
+      path: '/detail',
+      name: 'detail',
+      component: Detail
+    }
+
+
   ]
 })
 
---
+++ src/views/Detail.vue
@@ -0,0 +1,167 @@
+<script></script>
+
+<template>
+    <section class="bg-white">
+        <div class="lg:grid h-[calc(100vh-4rem)] lg:grid-cols-12">
+            <section class="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
+                <img alt=""
+                    src="https://images.unsplash.com/photo-1617195737496-bc30194e3a19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                    class="absolute inset-0 h-full w-full object-cover opacity-80" />
+
+                <div class="hidden lg:relative lg:block lg:p-12">
+
+                    <h2 class="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
+                        Welcome to Squid 🦑
+                    </h2>
+
+                    <p class="mt-4 leading-relaxed text-white/90">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
+                        quibusdam aperiam voluptatum.
+                    </p>
+                </div>
+            </section>
+
+            <main
+                class="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6 overflow-auto">
+                <div class="w-full h-[calc(100vh-4rem)]">
+
+                    <div class="flow-root rounded-lg border border-gray-100 py-3 shadow-sm">
+                        <dl class="-my-3 divide-y divide-gray-100 text-sm">
+                            <div class="grid grid-cols-1 gap-1 p-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4">
+                                <dt class="font-medium text-gray-900">Title</dt>
+                                <dd class="text-gray-700 sm:col-span-2">Mr</dd>
+                            </div>
+
+                            <div class="grid grid-cols-1 gap-1 p-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4">
+                                <dt class="font-medium text-gray-900">Name</dt>
+                                <dd class="text-gray-700 sm:col-span-2">
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et facilis debitis
+                                    explicabo
+                                    doloremque impedit nesciunt dolorem facere, dolor quasi veritatis quia fugit aperiam
+                                    aspernatur neque molestiae labore aliquam soluta architecto?
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et facilis debitis
+                                    explicabo
+                                    doloremque impedit nesciunt dolorem facere, dolor quasi veritatis quia fugit aperiam
+                                    aspernatur neque molestiae labore aliquam soluta architecto? Lorem ipsum dolor, sit
+                                    amet consectetur adipisicing elit. Et facilis debitis
+                                    explicabo
+                                    doloremque impedit nesciunt dolorem facere, dolor quasi veritatis quia fugit aperiam
+                                    aspernatur neque molestiae labore aliquam soluta architecto? Lorem ipsum dolor, sit
+                                    amet consectetur adipisicing elit. Et facilis debitis
+                                    explicabo
+                                    doloremque impedit nesciunt dolorem facere, dolor quasi veritatis quia fugit aperiam
+                                    aspernatur neque molestiae labore aliquam soluta architecto? Lorem ipsum dolor, sit
+                                    amet consectetur adipisicing elit. Et facilis debitis
+                                    explicabo
+                                    doloremque impedit nesciunt dolorem facere, dolor quasi veritatis quia fugit aperiam
+                                    aspernatur neque molestiae labore aliquam soluta architecto? Lorem ipsum dolor, sit
+                                    amet consectetur adipisicing elit. Et facilis debitis
+                                    explicabo
+                                    doloremque impedit nesciunt dolorem facere, dolor quasi veritatis quia fugit aperiam
+                                    aspernatur neque molestiae labore aliquam soluta architecto?
+                                </dd>
+                            </div>
+
+
+                            <div class="grid grid-cols-1 gap-1 p-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4">
+                                <dt class="font-medium text-gray-900">Bio</dt>
+                                <dd class="text-gray-700 sm:col-span-2">
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et facilis debitis
+                                    explicabo
+                                    doloremque impedit nesciunt dolorem facere, dolor quasi veritatis quia fugit aperiam
+                                    aspernatur neque molestiae labore aliquam soluta architecto?
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et facilis debitis
+                                    explicabo
+                                    doloremque impedit nesciunt dolorem facere, dolor quasi veritatis quia fugit aperiam
+                                    aspernatur neque molestiae labore aliquam soluta architecto? Lorem ipsum dolor, sit
+                                    amet consectetur adipisicing elit. Et facilis debitis
+                                    explicabo
+                                    doloremque impedit nesciunt dolorem facere, dolor quasi veritatis quia fugit aperiam
+                                    aspernatur neque molestiae labore aliquam soluta architecto? Lorem ipsum dolor, sit
+                                    amet consectetur adipisicing elit. Et facilis debitis
+                                    explicabo
+                                    doloremque impedit nesciunt dolorem facere, dolor quasi veritatis quia fugit aperiam
+                                    aspernatur neque molestiae labore aliquam soluta architecto? Lorem ipsum dolor, sit
+                                    amet consectetur adipisicing elit. Et facilis debitis
+                                    explicabo
+                                    doloremque impedit nesciunt dolorem facere, dolor quasi veritatis quia fugit aperiam
+                                    aspernatur neque molestiae labore aliquam soluta architecto? Lorem ipsum dolor, sit
+                                    amet consectetur adipisicing elit. Et facilis debitis
+                                    explicabo
+                                    doloremque impedit nesciunt dolorem facere, dolor quasi veritatis quia fugit aperiam
+                                    aspernatur neque molestiae labore aliquam soluta architecto?
+                                </dd>
+                            </div>
+
+                            <div class="grid grid-cols-1 gap-1 p-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4">
+                                <dt class="font-medium text-gray-900">Rating</dt>
+                                <dd class="text-gray-700 sm:col-span-2">
+                                    <!-- Rating -->
+                                    <div class="flex flex-row-reverse justify-end items-center">
+                                        <input id="hs-ratings-readonly-1" type="radio"
+                                            class="peer -ms-5 size-5 bg-transparent border-0 text-transparent cursor-pointer appearance-none checked:bg-none focus:bg-none focus:ring-0 focus:ring-offset-0"
+                                            name="hs-ratings-readonly" value="1">
+                                        <label for="hs-ratings-readonly-1"
+                                            class="peer-checked:text-yellow-400 text-gray-300 pointer-events-none dark:peer-checked:text-yellow-600 dark:text-gray-600">
+                                            <svg class="flex-shrink-0 size-5" xmlns="http://www.w3.org/2000/svg"
+                                                width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                                <path
+                                                    d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                                            </svg>
+                                        </label>
+                                        <input id="hs-ratings-readonly-2" type="radio"
+                                            class="peer -ms-5 size-5 bg-transparent border-0 text-transparent cursor-pointer appearance-none checked:bg-none focus:bg-none focus:ring-0 focus:ring-offset-0"
+                                            name="hs-ratings-readonly" value="2">
+                                        <label for="hs-ratings-readonly-2"
+                                            class="peer-checked:text-yellow-400 text-gray-300 pointer-events-none dark:peer-checked:text-yellow-600 dark:text-gray-600">
+                                            <svg class="flex-shrink-0 size-5" xmlns="http://www.w3.org/2000/svg"
+                                                width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                                <path
+                                                    d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                                            </svg>
+                                        </label>
+                                        <input id="hs-ratings-readonly-3" type="radio"
+                                            class="peer -ms-5 size-5 bg-transparent border-0 text-transparent cursor-pointer appearance-none checked:bg-none focus:bg-none focus:ring-0 focus:ring-offset-0"
+                                            name="hs-ratings-readonly" value="3">
+                                        <label for="hs-ratings-readonly-3"
+                                            class="peer-checked:text-yellow-400 text-gray-300 pointer-events-none dark:peer-checked:text-yellow-600 dark:text-gray-600">
+                                            <svg class="flex-shrink-0 size-5" xmlns="http://www.w3.org/2000/svg"
+                                                width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                                <path
+                                                    d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                                            </svg>
+                                        </label>
+                                        <input id="hs-ratings-readonly-4" type="radio"
+                                            class="peer -ms-5 size-5 bg-transparent border-0 text-transparent cursor-pointer appearance-none checked:bg-none focus:bg-none focus:ring-0 focus:ring-offset-0"
+                                            name="hs-ratings-readonly" value="4">
+                                        <label for="hs-ratings-readonly-4"
+                                            class="peer-checked:text-yellow-400 text-gray-300 pointer-events-none dark:peer-checked:text-yellow-600 dark:text-gray-600">
+                                            <svg class="flex-shrink-0 size-5" xmlns="http://www.w3.org/2000/svg"
+                                                width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                                <path
+                                                    d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                                            </svg>
+                                        </label>
+                                        <input id="hs-ratings-readonly-5" type="radio"
+                                            class="peer -ms-5 size-5 bg-transparent border-0 text-transparent cursor-pointer appearance-none checked:bg-none focus:bg-none focus:ring-0 focus:ring-offset-0"
+                                            name="hs-ratings-readonly" value="5">
+                                        <label for="hs-ratings-readonly-5"
+                                            class="peer-checked:text-yellow-400 text-gray-300 pointer-events-none dark:peer-checked:text-yellow-600 dark:text-gray-600">
+                                            <svg class="flex-shrink-0 size-5" xmlns="http://www.w3.org/2000/svg"
+                                                width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                                <path
+                                                    d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                                            </svg>
+                                        </label>
+                                    </div>
+                                    <!-- End Rating -->
+                                </dd>
+                            </div>
+
+                        </dl>
+                    </div>
+                </div>
+            </main>
+        </div>
+    </section>
+</template>
